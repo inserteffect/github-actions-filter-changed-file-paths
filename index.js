696fd04be3b7d4c73563b03e7239ae046dbdd75b
Florian Anselstetter <1913146+anselstetter@users.noreply.github.com>
2022-10-31 13:21:21 +0100

    Add exclude

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const changedFiles = core.getInput("changedFiles");
+  const changedFiles = JSON.parse(core.getInput("changedFiles"));
+  const exclude = JSON.parse(core.getInput("exclude"));
   const depth = core.getInput("depth");
 
   const filteredPaths = Array.from(
     new Set(
-      JSON.parse(changedFiles).map((e) => {
-        return e.split("/").slice(0, depth).join("/");
-      })
+      changedFiles
+        .map((e) => e.split("/").slice(0, depth).join("/"))
+        .filter((e) => !exclude.some((dir) => e.startsWith(dir)))
     )
   );
 
